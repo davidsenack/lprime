c8e2640a487ae12486454da175f95615eeee5448
David Senack <david.senack@gmail.com>
2024-03-24 19:34:00 -0400

    change path of lprime

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 N_VALUE=$1
-{ time /home/ubuntu/lprime/build-output/lprime -n $N_VALUE; } 2>&1 | tee  results.txt &
+{ time ./build-output/lprime -n $N_VALUE; } 2>&1 | tee  results.txt &
