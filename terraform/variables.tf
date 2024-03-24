48f27d3b339e5f599f1770fc5e68fc9b61590ed1
David Senack <david.senack@gmail.com>
2024-03-24 13:20:06 -0400

    add security key

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "my_ip_address" {
   description = "The IP address allowed for SSH access"
   type        = string
 }
+
+variable "instance_key_name" {
+  description = "The name of the EC2 key pair to use for SSH access"
+  type        = string
+}
