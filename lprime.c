837376be740240db633ad4629e2e4cd8fa4056be
David Senack <david.senack@gmail.com>
2024-03-24 11:51:38 -0400

    add command-line args

--- lprime.c
+++ lprime.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
+#include <string.h>
 
 void generateMersennePrime(int n, mpz_t *result) {
     mpz_init(*result);
@@ -8,18 +9,34 @@ void generateMersennePrime(int n, mpz_t *result) {
     mpz_sub_ui(*result, *result, 1); // 2^n - 1
 }
 
-int main(void) {
-    int n;
-    printf("Enter the value of n: ");
-    if (scanf("%d", &n) != 1) {
-        fprintf(stderr, "Failed to read the value of n.\n");
+int main(int argc, char *argv[]) {
+    int n = 0, k = 1; // Default values
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--number") == 0) {
+            if (i + 1 < argc) {
+                n = atoi(argv[++i]);
+            } else {
+                fprintf(stderr, "Option -n/--number requires an argument.\n");
+                return 1;
+            }
+        } else if (strcmp(argv[i], "-k") == 0 || strcmp(argv[i], "--rounds") == 0) {
+            if (i + 1 < argc) {
+                k = atoi(argv[++i]);
+            } else {
+                fprintf(stderr, "Option -k/--rounds requires an argument.\n");
+                return 1;
+            }
+        }
+    }
+
+    if (n == 0) {
+        fprintf(stderr, "The value of n was not provided.\n");
         return 1;
     }
 
     mpz_t mersennePrime;
     generateMersennePrime(n, &mersennePrime);
 
-    int k = 1; // Limit iterations to one for speed
     int result = mpz_probab_prime_p(mersennePrime, k);
     if (result == 2 || result == 1) {
         printf("The Mersenne number is probably prime.\n");
